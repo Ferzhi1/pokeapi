5c942499253541a5d31d3d505c5002604ac3bd89
Hugo Melo <Hugo.Melo@Foundever.com>
2025-05-08 10:15:31 -0500

    Vistas login y register

--- Api3/obj/Debug/net8.0/Api3.AssemblyInfo.cs
+++ Api3/obj/Debug/net8.0/Api3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74064d82b8db76a62e044389ea6bf4b6eeef854c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a632e02fbf49c186740dd9cd6ac3c40f41a1f15")]
 [assembly: System.Reflection.AssemblyProductAttribute("api3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
